130f9aea35a7daef243b99cec8d92d73c657ed75
causelovem <rat.taurus@gmail.com>
2018-04-18 21:57:40 +0300

    new matrix with new tests with trace prog

--- neuralKeras/src/predMatrix5.py
+++ neuralKeras/src/predMatrix5.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 
 from keras.models import Sequential, load_model
+import matplotlib.pyplot as plt
 
 np.set_printoptions(threshold=np.nan)
 
@@ -10,6 +11,10 @@ if (len(sys.argv) != 2):
     print('> Unexpected quantity of arguments, check your comand string.')
     sys.exit(1)
 
+classList = ['TRIPLE INVERSE DIAGONAL', 'DOUBLE DIRECT DIAGONAL',
+             'DOUBLE DIRECT + DOUBLE INVERSE DIAGONALS', 'UPPER TIANGLE',
+             'DOUBLE INVERSE DIAGONALS', 'LOWWER TIANGLE', 'RANDOM']
+
 matrixList = []
 
 fileIn = open(sys.argv[1], "r")
@@ -17,8 +22,9 @@ fileIn = open(sys.argv[1], "r")
 matrix = fileIn.readlines()
 dim = len(matrix)
 
+print('> Readind matrix data...')
 for i in range(dim):
-    matrix[i] = matrix[i][:-2].split(' ')
+    matrix[i] = matrix[i][:-1].split(' ')
     # for j in range(len(matrix[i])):
     #     matrix[i][j] = int(matrix[i][j])
 
@@ -34,6 +40,10 @@ for i in range(dim):
         else:
             matrix[i][pairList[j][0]] = j + 1
 
+# plt.imshow(matrix)
+# plt.colorbar()
+# plt.show()
+
 tmp = np.array(matrix)
 tmp = np.expand_dims(tmp, axis=2)
 matrixList.append(tmp)
@@ -48,11 +58,14 @@ print(matrixVec.shape)
 
 print('> Preparing for prediction...')
 model = Sequential()
-model = load_model('./nets/net1.h5')
-# model = load_model('./nets/netnetnet1.h5')
+# model = load_model('./nets/net1.h5')
+model = load_model('./nets/goodNet3.h5')
 
 print('> Predict on test data...')
 pred = model.predict(matrixVec[0:1])
 res = str(np.where(pred == pred.max())[1][0])
-resMsg = '> Your matrix class is {}'.format(res)
+
+print('> Probabilities of prediction are:')
+print(pred)
+resMsg = '> Your matrix class is {} ({})'.format(res, classList[int(res)])
 print(resMsg)
